32f567a1c3f27e8a3bc7f9593f3aeb849bb29ce3
Pedro Galan <pedro.galan@piksel.com>
2020-04-10 19:45:13 +0100

    Hello world HTTP server

--- src/my_first_clojure_web_app/core.clj
+++ src/my_first_clojure_web_app/core.clj
@@ -1,4 +1,13 @@
-(ns my-first-clojure-web-app.core)
+(ns my-first-clojure-web-app.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]))
+
+(defn handler [_]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello world"})
+
+(defn -main []
+  (run-jetty handler {:port 3000}))
 
 (defn foo
   "I don't do a whole lot."
